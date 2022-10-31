bfecdcb6d31fd0019f84c87c8bd852ba8e2f342b
Tyler Besecker <89785560+TylerBesecker@users.noreply.github.com>
2022-10-31 04:59:33 -0400

    Create ToARegions.java
    this looks right, but can't test until later lol

---
+++ src/main/java/com/tuna/toa/ToARegions.java
@@ -0,0 +1,33 @@
+package com.tuna.toa;
+
+public enum ToARegions
+{
+
+  MAIN_AREA(14160),
+  CHEST_ROOM(14672),
+  PUZZLE_MONKEY(15186),
+  PUZZLE_CRONDIS(15698),
+  PUZZLE_SCABARAS(14162),
+  PUZZLE_HET(14674),
+  BOSS_BABA(15188),
+  BOSS_ZEBAK(15700),
+  BOSS_KEPHRI(14164),
+  BOSS_AKKHA(14676),
+  BOSS_WARDEN(15184),
+  BOSS_WARDEN_FINAL(15696);
+
+  
+    public final int regionID;
+    private static final Map<int, ToARegions> REGION_MAP = new HashMap<>();
+    
+    static {
+        for (ToARegions e: values()) {
+            REGION_MAP.put(e.regionID, e);
+        }
+    }
+
+    public static ToARegions valueOfLabel(int regionID) {
+        return REGION_MAP.get(regionID);
+    }
+  
+}
